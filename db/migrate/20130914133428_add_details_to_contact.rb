5dad0d7e8fdad036b0b1091df355780556ba1363
Ramsés López <ramses.lopezs@gmail.com>
2013-09-15 17:03:21 -0430

    Se agregaron 4 campos en el formulario de contacto; no tiene funcionalidad aun

--- db/migrate/20130914133428_add_details_to_contact.rb
+++ db/migrate/20130914133428_add_details_to_contact.rb
@@ -1,4 +1,9 @@
 class AddDetailsToContact < ActiveRecord::Migration
   def change
+  	add_column :contacts, :event, :string
+  	add_column :contacts, :property_type, :string
+  	add_column :contacts, :property_location, :string
+  	add_column :contacts, :amount, :string
+  	add_column :contacts, :services, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121221033947) do
+ActiveRecord::Schema.define(:version => 20130914133428) do
 
   create_table "account_contacts", :force => true do |t|
     t.integer  "account_id"
@@ -146,29 +146,34 @@ ActiveRecord::Schema.define(:version => 20121221033947) do
     t.integer  "lead_id"
     t.integer  "assigned_to"
     t.integer  "reports_to"
-    t.string   "first_name",       :limit => 64,  :default => "",       :null => false
-    t.string   "last_name",        :limit => 64,  :default => "",       :null => false
-    t.string   "access",           :limit => 8,   :default => "Public"
-    t.string   "title",            :limit => 64
-    t.string   "department",       :limit => 64
-    t.string   "source",           :limit => 32
-    t.string   "email",            :limit => 64
-    t.string   "alt_email",        :limit => 64
-    t.string   "phone",            :limit => 32
-    t.string   "mobile",           :limit => 32
-    t.string   "fax",              :limit => 32
-    t.string   "blog",             :limit => 128
-    t.string   "linkedin",         :limit => 128
-    t.string   "facebook",         :limit => 128
-    t.string   "twitter",          :limit => 128
+    t.string   "first_name",        :limit => 64,  :default => "",       :null => false
+    t.string   "last_name",         :limit => 64,  :default => "",       :null => false
+    t.string   "access",            :limit => 8,   :default => "Public"
+    t.string   "title",             :limit => 64
+    t.string   "department",        :limit => 64
+    t.string   "source",            :limit => 32
+    t.string   "email",             :limit => 64
+    t.string   "alt_email",         :limit => 64
+    t.string   "phone",             :limit => 32
+    t.string   "mobile",            :limit => 32
+    t.string   "fax",               :limit => 32
+    t.string   "blog",              :limit => 128
+    t.string   "linkedin",          :limit => 128
+    t.string   "facebook",          :limit => 128
+    t.string   "twitter",           :limit => 128
     t.date     "born_on"
-    t.boolean  "do_not_call",                     :default => false,    :null => false
+    t.boolean  "do_not_call",                      :default => false,    :null => false
     t.datetime "deleted_at"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                                             :null => false
+    t.datetime "updated_at",                                             :null => false
     t.string   "background_info"
-    t.string   "skype",            :limit => 128
+    t.string   "skype",             :limit => 128
     t.text     "subscribed_users"
+    t.string   "event"
+    t.string   "property_type"
+    t.string   "property_location"
+    t.string   "amount"
+    t.string   "services"
   end
 
   add_index "contacts", ["assigned_to"], :name => "index_contacts_on_assigned_to"
